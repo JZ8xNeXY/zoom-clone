8073533e204182dddbca9516c67d5161d6f55723
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2025-10-26 13:26:31 +0900

    サインアップサインイン機能の導入

---
+++ zoom-clone/src/modules/auth/auth.repository.ts
@@ -0,0 +1,26 @@
+import api from "../../lib/api"
+import { User } from "../users/user.entity"
+
+
+export const authRepository = {
+  async signup(name:string, email:string, password:string) : Promise<{user:User,token:string}>{
+    const result = await api.post('auth/signup',{
+      name,
+      email,
+      password
+    })
+
+    const {user, token} = result.data
+    return {user:new User(user),token}
+  },
+
+  async signIn(email:string,password:string){
+    const result = await api.post('auth/signin',{
+      email,
+      password
+    })
+
+    const {user,token} = result.data
+    return {user:new User(user),token}
+  },
+}
